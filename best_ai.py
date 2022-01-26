a841762f8ecd6ecce00a0b1ffc8deb0723bb3cdc
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2022-01-26 11:26:39 -0500

    changed spaces to tabs.....why dr. eicholtz

--- best_ai.py
+++ best_ai.py
@@ -4,41 +4,41 @@ import utils
 
 
 def makeguess(wordlist, guesses=[], feedback=[]):
-    """Guess a word from the available wordlist, (optionally) using feedback 
-    from previous guesses.
-    
-    Parameters
-    ----------
-    wordlist : list of str
-        A list of the valid word choices. The output must come from this list.
-    guesses : list of str
-        A list of the previously guessed words, in the order they were made, 
-        e.g. guesses[0] = first guess, guesses[1] = second guess. The length 
-        of the list equals the number of guesses made so far. An empty list 
-        (default) implies no guesses have been made.
-    feedback : list of lists of int
-        A list comprising one list per word guess and one integer per letter 
-        in that word, to indicate if the letter is correct (2), almost 
-        correct (1), or incorrect (0). An empty list (default) implies no 
-        guesses have been made.
+	"""Guess a word from the available wordlist, (optionally) using feedback 
+	from previous guesses.
 
-    Output
-    ------
-    word : str
-        The word chosen by the AI for the next guess.
-    """
+	Parameters
+	----------
+	wordlist : list of str
+		A list of the valid word choices. The output must come from this list.
+	guesses : list of str
+		A list of the previously guessed words, in the order they were made, 
+		e.g. guesses[0] = first guess, guesses[1] = second guess. The length 
+		of the list equals the number of guesses made so far. An empty list 
+		(default) implies no guesses have been made.
+	feedback : list of lists of int
+		A list comprising one list per word guess and one integer per letter 
+		in that word, to indicate if the letter is correct (2), almost 
+		correct (1), or incorrect (0). An empty list (default) implies no 
+		guesses have been made.
 
-
-	#
-
+	Output
+	------
+	word : str
+		The word chosen by the AI for the next guess.
+	"""
+	if(len(guesses) == 0):
+		return "adieu"
 
 
 
 
 
-    return random.choice(wordlist)
+
+
+	return random.choice(wordlist)
 
 
 if __name__ == "__main__":
-    wordlist = utils.readwords("allwords5.txt")
-    print(f"AI: 'My next choice would be {makeguess(wordlist)}'")
+	wordlist = utils.readwords("allwords5.txt")
+	print(f"AI: 'My next choice would be {makeguess(wordlist)}'")
